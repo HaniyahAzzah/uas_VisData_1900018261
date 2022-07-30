7cb88ea55b44412135caf6527d33bcf8aa8596fb
HaniyahAzzah <80875124+HaniyahAzzah@users.noreply.github.com>
2022-07-30 18:59:55 +0700

    Update HaniyahAzzahUlfah-1900018261-UAS_VisDat_genap2021.py

--- HaniyahAzzahUlfah-1900018261-UAS_VisDat_genap2021.py
+++ HaniyahAzzahUlfah-1900018261-UAS_VisDat_genap2021.py
@@ -1,4 +1,4 @@
-from turtle import left, right
+
 import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
